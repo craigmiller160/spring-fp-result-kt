2090521269f8f8b20e29a5a128e3e2fc258c9e61
Craig Miller <craigmiller160@gmail.com>
2022-06-11 16:39:59 -0400

    Progress

--- src/main/kotlin/io/craigmiller160/springarrowkt/EitherMethodReturnValueHandler.kt
+++ src/main/kotlin/io/craigmiller160/springarrowkt/EitherMethodReturnValueHandler.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.springarrowkt
 
 import arrow.core.Either
 import org.springframework.core.MethodParameter
+import org.springframework.http.ResponseEntity
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler
 import org.springframework.web.method.support.ModelAndViewContainer
@@ -18,14 +19,23 @@ class EitherMethodReturnValueHandler(private val delegate: HandlerMethodReturnVa
       returnType: MethodParameter,
       mavContainer: ModelAndViewContainer,
       webRequest: NativeWebRequest
-  ) {
-    when (returnValue) {
-      is Either.Left<*> -> handleLeft(returnValue.value)
-      is Either.Right<*> ->
-          delegate.handleReturnValue(returnValue.value, returnType, mavContainer, webRequest)
-      else -> delegate.handleReturnValue(returnValue, returnType, mavContainer, webRequest)
-    }
-  }
+  ) =
+      when (returnValue) {
+        is Either.Left<*> -> handleLeft(returnValue.value)
+        is Either.Right<*> -> handleRight(returnValue.value, returnType, mavContainer, webRequest)
+        else -> delegate.handleReturnValue(returnValue, returnType, mavContainer, webRequest)
+      }
+
+  private fun handleRight(
+      rightValue: Any?,
+      returnType: MethodParameter,
+      mavContainer: ModelAndViewContainer,
+      webRequest: NativeWebRequest
+  ) =
+      when (rightValue) {
+        is ResponseEntity<*> -> TODO("Not implemented")
+        else -> delegate.handleReturnValue(rightValue, returnType, mavContainer, webRequest)
+      }
 
   private fun handleLeft(leftValue: Any?): Nothing =
       when (leftValue) {
