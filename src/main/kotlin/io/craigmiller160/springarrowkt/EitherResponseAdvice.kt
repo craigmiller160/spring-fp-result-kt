68ebfa6687853f87e381d6108982db16ee483565
Craig Miller <craigmiller160@gmail.com>
2022-06-11 14:26:25 -0400

    Got initial error handling for Either working

--- src/main/kotlin/io/craigmiller160/springarrowkt/EitherResponseAdvice.kt
+++ src/main/kotlin/io/craigmiller160/springarrowkt/EitherResponseAdvice.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.springarrowkt
 
 import arrow.core.Either
+import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.AfterReturning
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
@@ -10,18 +11,16 @@ import org.springframework.stereotype.Component
 @Aspect
 class EitherResponseAdvice {
   @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
-  fun controllerMethods() {}
+  fun restController() {}
 
-  @AfterReturning("controllerMethods()")
-  fun experiment(value: Any?) {
-    value?.let { nonNullValue ->
+  @AfterReturning(value = "restController()", returning = "returnValue")
+  fun experiment(joinPoint: JoinPoint, returnValue: Any?) {
+    returnValue?.let { nonNullValue ->
       if (nonNullValue is Either.Left<*>) {
         nonNullValue.value?.let { eitherValue ->
           if (eitherValue is Throwable) {
             throw eitherValue
           }
-
-          // TODO improve this
           throw EitherLeftException(eitherValue.toString())
         }
       }
