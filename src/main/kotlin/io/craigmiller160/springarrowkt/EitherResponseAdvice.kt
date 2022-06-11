03581769fdc76ae0756f5933e5e50547d2fb2b96
Craig Miller <craigmiller160@gmail.com>
2022-06-11 14:39:07 -0400

    Testing around() solution

--- src/main/kotlin/io/craigmiller160/springarrowkt/EitherResponseAdvice.kt
+++ src/main/kotlin/io/craigmiller160/springarrowkt/EitherResponseAdvice.kt
@@ -2,7 +2,8 @@ package io.craigmiller160.springarrowkt
 
 import arrow.core.Either
 import org.aspectj.lang.JoinPoint
-import org.aspectj.lang.annotation.AfterReturning
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
@@ -13,18 +14,30 @@ class EitherResponseAdvice {
   @Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
   fun restController() {}
 
-  @AfterReturning(value = "restController()", returning = "returnValue")
+  // TODO rename if it works
+  //  @AfterReturning(value = "restController()", returning = "returnValue")
   fun experiment(joinPoint: JoinPoint, returnValue: Any?): Any? =
       returnValue?.let(::handleReturnValue)
+
+  // TODO rename if it works
+  @Around("restController()")
+  fun experiment2(joinPoint: ProceedingJoinPoint): Any? {
+    val returnValue = joinPoint.proceed()
+    return returnValue?.let(::handleReturnValue)
+  }
 
   private fun handleReturnValue(returnValue: Any): Any? =
       when (returnValue) {
         is Either.Left<*> -> handleLeft(returnValue.value)
-        is Either.Right<*> -> returnValue.value
+        is Either.Right<*> -> handleRight(returnValue.value)
         else ->
             throw IllegalStateException(
                 "Invalid Either return value type: ${returnValue.javaClass.name}")
       }
+
+  private fun handleRight(rightValue: Any?): Any? {
+    return mapOf("foo" to "bar")
+  }
 
   private fun handleLeft(leftValue: Any?): Nothing =
       when (leftValue) {
