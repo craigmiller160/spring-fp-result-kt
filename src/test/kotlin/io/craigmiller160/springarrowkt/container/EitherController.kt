63e44c8f23893bcddcc3161bb5bc0f5a3a22b240
Craig Miller <craigmiller160@gmail.com>
2022-06-11 10:50:22 -0400

    Stubbed out controller

---
+++ src/test/kotlin/io/craigmiller160/springarrowkt/container/EitherController.kt
@@ -0,0 +1,19 @@
+package io.craigmiller160.springarrowkt.container
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/either")
+class EitherController {
+    @GetMapping("/success")
+    fun success() {
+        TODO()
+    }
+
+    @GetMapping("/failure")
+    fun failure() {
+        TODO()
+    }
+}
