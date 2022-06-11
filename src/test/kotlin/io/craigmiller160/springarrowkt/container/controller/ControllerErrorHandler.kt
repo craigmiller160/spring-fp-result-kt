25b228c9c995c97a3c27e11c3e93efe64a6a6c68
Craig Miller <craigmiller160@gmail.com>
2022-06-11 11:05:09 -0400

    Added error handler

---
+++ src/test/kotlin/io/craigmiller160/springarrowkt/container/controller/ControllerErrorHandler.kt
@@ -0,0 +1,13 @@
+package io.craigmiller160.springarrowkt.container.controller
+
+import io.craigmiller160.springarrowkt.container.dto.ErrorResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class ControllerErrorHandler {
+  @ExceptionHandler(Exception::class)
+  fun exceptionHandler(ex: Exception): ResponseEntity<ErrorResponse> =
+      ResponseEntity.status(500).body(ErrorResponse(ex.message ?: ex.javaClass.name))
+}
