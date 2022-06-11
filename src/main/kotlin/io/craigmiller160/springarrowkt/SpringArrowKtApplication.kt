6a705056caea470633c91121c1fa9c2411546ab5
Craig Miller <craigmiller160@gmail.com>
2022-06-11 10:49:35 -0400

    Initial commit

---
+++ src/main/kotlin/io/craigmiller160/springarrowkt/SpringArrowKtApplication.kt
@@ -0,0 +1,11 @@
+package io.craigmiller160.springarrowkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringArrowKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringArrowKtApplication>(*args)
+}
