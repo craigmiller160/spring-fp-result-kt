4f90e3169f7e1b5bccae77ffcdf7e81f0730afe5
Craig Miller <craigmiller160@gmail.com>
2022-06-11 21:23:17 -0400

    Implemented tests

--- src/test/kotlin/io/craigmiller160/springarrowkt/transaction/EitherTransactionRollbackTest.kt
+++ src/test/kotlin/io/craigmiller160/springarrowkt/transaction/EitherTransactionRollbackTest.kt
@@ -1,7 +1,12 @@
 package io.craigmiller160.springarrowkt.transaction
 
+import io.craigmiller160.springarrowkt.container.Person
+import io.craigmiller160.springarrowkt.container.PersonRepository
 import io.craigmiller160.springarrowkt.container.TestApplication
 import io.craigmiller160.springarrowkt.container.service.PersonService
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,14 +17,31 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @ExtendWith(SpringExtension::class)
 class EitherTransactionRollbackTest {
   @Autowired private lateinit var personService: PersonService
+  @Autowired private lateinit var personRepository: PersonRepository
+
+  @BeforeEach
+  fun setup() {
+    personRepository.deleteAll()
+  }
+
+  @BeforeEach
+  fun cleanup() {
+    personRepository.deleteAll()
+  }
 
   @Test
   fun `commit if Right returned`() {
-    TODO()
+    val person = Person(name = "Bob", age = 20)
+    val result = personService.saveAndCommit(person)
+    // TODO validate that the result is a Right with the same value
+    assertNotNull(personRepository.findById(person.id))
   }
 
   @Test
   fun `rollback if Left returned`() {
-    TODO("")
+    val person = Person(name = "John", age = 30)
+    val result = personService.saveAndRollback(person)
+    // TODO validate that result is a Left
+    assertNull(personRepository.findById(person.id))
   }
 }
