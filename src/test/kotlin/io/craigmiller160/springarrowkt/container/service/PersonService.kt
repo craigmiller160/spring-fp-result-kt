a02aa2f091805a2ad2649a1e70340585e2085af1
Craig Miller <craigmiller160@gmail.com>
2022-06-11 21:18:16 -0400

    Added initial test service methods

--- src/test/kotlin/io/craigmiller160/springarrowkt/container/service/PersonService.kt
+++ src/test/kotlin/io/craigmiller160/springarrowkt/container/service/PersonService.kt
@@ -1,6 +1,20 @@
 package io.craigmiller160.springarrowkt.container.service
 
+import arrow.core.Either
+import io.craigmiller160.springarrowkt.container.Person
 import io.craigmiller160.springarrowkt.container.PersonRepository
+import javax.transaction.Transactional
 import org.springframework.stereotype.Service
 
-@Service class PersonService(private val personRepository: PersonRepository)
+@Service
+class PersonService(private val personRepository: PersonRepository) {
+  @Transactional
+  fun saveAndCommit(person: Person): Either<Throwable, Person> =
+      Either.Right(personRepository.save(person))
+
+  @Transactional
+  fun saveAndRollback(person: Person): Either<Throwable, Person> {
+    personRepository.save(person)
+    return Either.Left(RuntimeException("Dying"))
+  }
+}
