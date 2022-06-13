a21a4838c72c8b84596a11147fae453a12302f61
Craig Miller <craigmiller160@gmail.com>
2022-06-12 21:20:55 -0400

    Cleanup

--- src/test/kotlin/io/craigmiller160/springarrowkt/container/service/CompanyTransactionService.kt
+++ src/test/kotlin/io/craigmiller160/springarrowkt/container/service/CompanyTransactionService.kt
@@ -3,27 +3,11 @@ package io.craigmiller160.springarrowkt.container.service
 import io.craigmiller160.springarrowkt.container.config.H2DataSourceTwoConfig
 import io.craigmiller160.springarrowkt.container.domain.ds2.entities.Company
 import io.craigmiller160.springarrowkt.container.domain.ds2.repositories.CompanyRepository
-import java.util.UUID
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class CompanyTransactionService(
-    private val companyRepository: CompanyRepository,
-    private val nestedService: NestedCompanyTransactionService
-) {
+class CompanyTransactionService(private val companyRepository: CompanyRepository) {
   @Transactional(transactionManager = H2DataSourceTwoConfig.TXN_MANAGER)
   fun save(company: Company): Company = companyRepository.save(company)
-
-  @Transactional(transactionManager = H2DataSourceTwoConfig.TXN_MANAGER)
-  fun nestedSave(initCompany: Company): Pair<Company, Company?> {
-    val company = companyRepository.save(initCompany)
-    val otherCompany =
-        runCatching {
-              nestedService.anotherSave(
-                  initCompany.copy(id = UUID.randomUUID(), name = "${initCompany.name}-2"))
-            }
-            .getOrNull()
-    return Pair(company, otherCompany)
-  }
 }
