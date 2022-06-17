143665e5c5d603491bd3e7d40181f3f1c38d780c
Craig Miller <craigmiller160@gmail.com>
2022-06-17 09:01:07 -0400

    Got some tests working again

--- src/test/kotlin/io/github/craigmiller160/springarrowkt/container/TestApplication.kt
+++ src/test/kotlin/io/github/craigmiller160/springarrowkt/container/TestApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["io.craigmiller160.springarrowkt"])
+@ComponentScan(basePackages = ["io.github.craigmiller160.springarrowkt.container"])
 class TestApplication
 
 fun main(args: Array<String>) {
