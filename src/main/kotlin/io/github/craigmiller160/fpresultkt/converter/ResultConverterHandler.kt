817e75e338dd3120f14d775aa9112b412ee40cc1
Craig Miller <craigmiller160@gmail.com>
2023-03-02 18:57:48 -0500

    Better handling null values

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/ResultConverterHandler.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/ResultConverterHandler.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class ResultConverterHandler(private val eitherResultConverter: ResultConverter) {
-  fun convert(value: Any): CommonResult =
+  fun convert(value: Any?): CommonResult =
       when (ResultConverterStrategyFinder.find(value)) {
         ResultConverterStrategy.EITHER -> eitherResultConverter.convert(value)
         else -> CommonResultOther(value)
--- src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/ResultConverterStrategyFinder.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/ResultConverterStrategyFinder.kt
@@ -3,7 +3,11 @@ package io.github.craigmiller160.fpresultkt.converter
 object ResultConverterStrategyFinder {
   private val EITHER_CLASSES =
       listOf("arrow.core.Either", "arrow.core.Either\$Right", "arrow.core.Either\$Left")
-  fun find(value: Any): ResultConverterStrategy {
+  fun find(value: Any?): ResultConverterStrategy {
+    if (value == null) {
+      return ResultConverterStrategy.OTHER
+    }
+
     val valueClassName = value.javaClass.name
     return if (EITHER_CLASSES.contains(valueClassName)) {
       ResultConverterStrategy.EITHER
