dc1797bdd9b98926c010ded0e760d23bfe8abaa5
Craig Miller <craigmiller160@gmail.com>
2023-03-02 19:11:37 -0500

    Stubbed out a lot of tests

---
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/converter/ResultConverterHandlerTest.kt
@@ -0,0 +1,45 @@
+package io.github.craigmiller160.fpresultkt.converter
+
+import org.junit.jupiter.api.Test
+
+class ResultConverterHandlerTest {
+  @Test
+  fun `converts Either Right`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts Either Left`() {
+    TODO()
+  }
+
+  @Test
+  fun `converters non-result value`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts null value`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts Kotlin Result Ok`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts Kotlin Result Err`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts stdlib Result success`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts stdlib Result failure`() {
+    TODO()
+  }
+}
