efad7bec98929df2d54cdce2acc79d1f565746de
Craig Miller <craigmiller160@gmail.com>
2023-03-02 20:05:58 -0500

    Implemented another converter

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/stdlibresult/StdlibResultConverter.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/stdlibresult/StdlibResultConverter.kt
@@ -1,7 +1,10 @@
 package io.github.craigmiller160.fpresultkt.converter.stdlibresult
 
 import io.github.craigmiller160.fpresultkt.converter.CommonResult
+import io.github.craigmiller160.fpresultkt.converter.CommonResultFailure
+import io.github.craigmiller160.fpresultkt.converter.CommonResultSuccess
 import io.github.craigmiller160.fpresultkt.converter.ResultConverter
+import io.github.craigmiller160.fpresultkt.converter.ResultConverterStrategy
 import io.github.craigmiller160.fpresultkt.converter.STDLIB_RESULT_CLASS
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.stereotype.Component
@@ -10,6 +13,11 @@ import org.springframework.stereotype.Component
 @ConditionalOnClass(name = [STDLIB_RESULT_CLASS])
 class StdlibResultConverter : ResultConverter {
   override fun convert(value: Any): CommonResult {
-    TODO("Not yet implemented")
+    val result = value as kotlin.Result<*>
+    return if (result.isSuccess) {
+      CommonResultSuccess(result.getOrNull(), ResultConverterStrategy.STDLIB_RESULT)
+    } else {
+      CommonResultFailure(result.exceptionOrNull(), ResultConverterStrategy.STDLIB_RESULT)
+    }
   }
 }
