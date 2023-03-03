ec7d28b982f6904d399b8fe857bfcd449d81de8d
Craig Miller <craigmiller160@gmail.com>
2023-03-02 19:54:22 -0500

    Cleanup

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/ResultConverterStrategyFinder.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/ResultConverterStrategyFinder.kt
@@ -1,8 +1,6 @@
 package io.github.craigmiller160.fpresultkt.converter
 
 object ResultConverterStrategyFinder {
-  private val EITHER_CLASSES =
-      listOf("arrow.core.Either", "arrow.core.Either\$Right", "arrow.core.Either\$Left")
   fun find(value: Any?): ResultConverterStrategy {
     if (value == null) {
       return ResultConverterStrategy.OTHER
