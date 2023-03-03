1caa06e354e839bfc61cbc55f17fab23623ff453
Craig Miller <craigmiller160@gmail.com>
2023-03-02 19:09:26 -0500

    Cleanup

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/CommonResult.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/converter/CommonResult.kt
@@ -1,6 +1,5 @@
 package io.github.craigmiller160.fpresultkt.converter
 
-// TODO what about the original result type?
 sealed interface CommonResult {
   val value: Any?
 }
@@ -9,5 +8,4 @@ data class CommonResultSuccess(override val value: Any?) : CommonResult
 
 data class CommonResultFailure(override val value: Any?) : CommonResult
 
-// TODO unclear if this is necessary
 data class CommonResultOther(override val value: Any?) : CommonResult
