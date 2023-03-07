8b32f694fd3d8b081dd8e1bccae9ee6a18a93f11
Craig Miller <craigmiller160@gmail.com>
2023-03-07 07:42:58 -0500

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,28 +26,6 @@ tasks.getByName<Jar>("jar") {
     archiveClassifier.set("")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = rootProject.name
-            version = project.version.toString()
-
-            from(components["kotlin"])
-        }
-    }
-    repositories {
-        maven {
-            val repo = if (project.version.toString().endsWith("-SNAPSHOT")) "maven-snapshots" else "maven-releases"
-            url = uri("https://nexus-craigmiller160.ddns.net/repository/$repo")
-            credentials {
-                username = System.getenv("NEXUS_USER")
-                password = System.getenv("NEXUS_PASSWORD")
-            }
-        }
-    }
-}
-
 repositories {
     mavenCentral()
     maven {
