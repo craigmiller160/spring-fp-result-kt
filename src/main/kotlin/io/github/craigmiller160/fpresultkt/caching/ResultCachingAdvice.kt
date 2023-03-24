4135f239e77161e4caf1fd000c4dd465e134d13f
Craig Miller <craigmiller160@gmail.com>
2023-03-24 18:10:26 -0400

    Fixed startup

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCachingAdvice.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCachingAdvice.kt
@@ -6,13 +6,13 @@ import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.support.AbstractApplicationContext
 import org.springframework.stereotype.Component
 
 @Aspect
 @Component
 class ResultCachingAdvice(
-    private val context: AnnotationConfigApplicationContext,
+    private val context: AbstractApplicationContext,
     private val resultConverterHandler: ResultConverterHandler
 ) {
   @Pointcut("@annotation(org.springframework.cache.annotation.Cacheable)") fun cacheable() {}
