444cd2bf2b2349bac8df689f44ac21a19d30cfbd
Craig Miller <craigmiller160@gmail.com>
2023-03-24 17:57:25 -0400

    Stubbed out tests

--- src/test/kotlin/io/github/craigmiller160/fpresultkt/caching/EitherCachingTest.kt
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/caching/EitherCachingTest.kt
@@ -1,5 +1,16 @@
 package io.github.craigmiller160.fpresultkt.caching
 
 import io.github.craigmiller160.fpresultkt.BaseTest
+import org.junit.jupiter.api.Test
 
-class EitherCachingTest : BaseTest() {}
+class EitherCachingTest : BaseTest() {
+  @Test
+  fun `stores the value in the cache for a Right`() {
+    TODO()
+  }
+
+  @Test
+  fun `does not store the value in the cache for a Left`() {
+    TODO()
+  }
+}
