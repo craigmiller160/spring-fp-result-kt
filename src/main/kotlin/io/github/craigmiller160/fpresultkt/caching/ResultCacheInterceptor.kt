a491459e13747ae3b196d9fb2b05b80b4393458b
Craig Miller <craigmiller160@gmail.com>
2023-03-24 19:31:49 -0400

    Trying to make progress

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCacheInterceptor.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCacheInterceptor.kt
@@ -1,15 +1,44 @@
 package io.github.craigmiller160.fpresultkt.caching
 
+import java.lang.reflect.Method
 import org.aopalliance.intercept.MethodInvocation
+import org.springframework.aop.framework.AopProxyUtils
 import org.springframework.cache.interceptor.CacheInterceptor
+import org.springframework.cache.interceptor.CacheOperationInvoker
 import org.springframework.cache.interceptor.CacheOperationSource
 
 class ResultCacheInterceptor(private val defaultCacheInterceptor: CacheInterceptor) :
     CacheInterceptor() {
+  private var initialized: Boolean = false
   override fun invoke(invocation: MethodInvocation): Any? {
     val result = super.invoke(invocation)
-    TODO()
+    return result
   }
+
+  override fun afterSingletonsInstantiated() {
+    super.afterSingletonsInstantiated()
+    initialized = true
+  }
+
+  override fun execute(
+      invoker: CacheOperationInvoker,
+      target: Any,
+      method: Method,
+      args: Array<Any>
+  ): Any? {
+    if (!initialized) {
+      return invoker.invoke()
+    }
+
+    val targetClass = AopProxyUtils.ultimateTargetClass(target)
+    cacheOperationSource?.getCacheOperations(method, targetClass)?.let { operations ->
+      val context = CacheOperationContexts(operations, method, args, target, targetClass)
+    }
+
+    return super.execute(invoker, target, method, args)
+  }
+
+  private fun updateCache(invocation: MethodInvocation, result: Any?) {}
 
   override fun getCacheOperationSource(): CacheOperationSource? =
       defaultCacheInterceptor.cacheOperationSource
