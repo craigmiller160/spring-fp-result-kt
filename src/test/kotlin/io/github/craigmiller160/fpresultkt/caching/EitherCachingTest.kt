bfa30e2423d8b2977909946e56353faf6c4ce43c
Craig Miller <craigmiller160@gmail.com>
2023-03-24 17:59:57 -0400

    Fixed tests

--- src/test/kotlin/io/github/craigmiller160/fpresultkt/caching/EitherCachingTest.kt
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/caching/EitherCachingTest.kt
@@ -2,8 +2,8 @@ package io.github.craigmiller160.fpresultkt.caching
 
 import io.github.craigmiller160.fpresultkt.BaseTest
 import io.github.craigmiller160.fpresultkt.container.service.CachingService
+import io.kotest.assertions.arrow.core.shouldBeRight
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -28,16 +28,16 @@ class EitherCachingTest : BaseTest() {
   @Test
   fun `stores the value in the cache for a Right`() {
     cachingService.count = 1
-    assertEquals(1, cachingService.getCountWithCache(false))
+    cachingService.getCountWithCache(false).shouldBeRight(1)
     cachingService.count = 10
-    assertEquals(1, cachingService.getCountWithCache(false))
+    cachingService.getCountWithCache(false).shouldBeRight(1)
   }
 
   @Test
   fun `does not store the value in the cache for a Left`() {
     cachingService.count = 1
-    assertEquals(1, cachingService.getCountWithCache(true))
+    cachingService.getCountWithCache(true).shouldBeRight(1)
     cachingService.count = 10
-    assertEquals(10, cachingService.getCountWithCache(false))
+    cachingService.getCountWithCache(false).shouldBeRight(10)
   }
 }
