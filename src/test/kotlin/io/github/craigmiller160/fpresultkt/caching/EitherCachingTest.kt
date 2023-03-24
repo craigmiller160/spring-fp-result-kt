7d6538a4e274af2b29bac41aad51a2e007f78eef
Craig Miller <craigmiller160@gmail.com>
2023-03-24 17:56:22 -0400

    MOre test prep

---
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/caching/EitherCachingTest.kt
@@ -0,0 +1,5 @@
+package io.github.craigmiller160.fpresultkt.caching
+
+import io.github.craigmiller160.fpresultkt.BaseTest
+
+class EitherCachingTest : BaseTest() {}
--- src/test/kotlin/io/github/craigmiller160/fpresultkt/container/service/CachingService.kt
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/container/service/CachingService.kt
@@ -1,5 +1,6 @@
 package io.github.craigmiller160.fpresultkt.container.service
 
+import arrow.core.Either
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
@@ -11,7 +12,13 @@ class CachingService {
   }
   var count: Int = 0
 
-  @Cacheable(cacheNames = [CACHE]) fun getCountWithCache(throwException: Boolean): Int = count
+  @Cacheable(cacheNames = [CACHE])
+  fun getCountWithCache(withException: Boolean): Either<Throwable, Int> {
+    if (withException) {
+      return Either.Left(RuntimeException("Dying"))
+    }
+    return Either.Right(count)
+  }
 
   @CacheEvict(cacheNames = [CACHE], allEntries = true) fun clearCache() {}
 }
