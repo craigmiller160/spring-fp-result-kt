95b63ad12d9fa9c93308a039004e4f09de09757f
Craig Miller <craigmiller160@gmail.com>
2023-03-25 10:51:22 -0400

    Cleanup

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCachingConfig.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCachingConfig.kt
@@ -2,19 +2,15 @@ package io.github.craigmiller160.fpresultkt.caching
 
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.cache.interceptor.CacheInterceptor
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Role
-import org.springframework.core.annotation.Order
 
-@Order(999)
-@Configuration(proxyBeanMethods = false)
+// @Order(999)
+// @Configuration(proxyBeanMethods = false)
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 class ResultCachingConfig {
   // TODO delete this
-  @Bean
-  @Primary
+  //  @Bean
+  //  @Primary
   @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
   fun resultCacheInterceptor(defaultCacheInterceptor: CacheInterceptor): CacheInterceptor {
     return ResultCacheInterceptor(defaultCacheInterceptor)
