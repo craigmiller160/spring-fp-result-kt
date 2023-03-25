006b644874ff1c49d49136c78efb70845ee7b81a
Craig Miller <craigmiller160@gmail.com>
2023-03-25 10:59:26 -0400

    Implemented new cache logic

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCache.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/caching/ResultCache.kt
@@ -1,11 +1,17 @@
 package io.github.craigmiller160.fpresultkt.caching
 
+import io.github.craigmiller160.fpresultkt.converter.CommonResultFailure
 import io.github.craigmiller160.fpresultkt.converter.ResultConverterHandler
 import org.springframework.cache.Cache
 
-class ResultCache(private val resultConverterHandler: ResultConverterHandler, cache: Cache) :
-    Cache by cache {
+class ResultCache(
+    private val resultConverterHandler: ResultConverterHandler,
+    private val cache: Cache
+) : Cache by cache {
   override fun put(key: Any, value: Any?) {
-    TODO("Not yet implemented")
+    val commonResult = resultConverterHandler.convert(value)
+    if (commonResult !is CommonResultFailure) {
+      cache.put(key, value)
+    }
   }
 }
