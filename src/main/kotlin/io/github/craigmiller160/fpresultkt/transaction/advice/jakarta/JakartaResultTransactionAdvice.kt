fd80ce39da75461e176b429dd9e4dcb67f495d82
Craig Miller <craigmiller160@gmail.com>
2023-04-01 18:42:11 -0400

    Implemented one new advice class

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/transaction/advice/jakarta/JakartaResultTransactionAdvice.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/transaction/advice/jakarta/JakartaResultTransactionAdvice.kt
@@ -1,6 +1,20 @@
 package io.github.craigmiller160.fpresultkt.transaction.advice.jakarta
 
+import io.github.craigmiller160.fpresultkt.transaction.support.ResultTransactionSupport
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
 
-@Aspect @Component class JakartaResultTransactionAdvice {}
+@Aspect
+@Component
+class JakartaResultTransactionAdvice(
+    private val resultTransactionSupport: ResultTransactionSupport
+) {
+  @Pointcut("@annotation(jakarta.transaction.Transactional)") fun jakartaTransactional() {}
+
+  @Around("jakartaTransactional()")
+  fun handleResultReturnValue(joinPoint: ProceedingJoinPoint): Any? =
+      resultTransactionSupport.handleResultReturnValue(joinPoint)
+}
