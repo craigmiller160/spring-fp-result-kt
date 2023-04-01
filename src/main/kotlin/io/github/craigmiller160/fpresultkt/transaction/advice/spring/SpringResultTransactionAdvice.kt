75a638520ea99e3b38f185f1cb537abe883dfb31
Craig Miller <craigmiller160@gmail.com>
2023-04-01 18:43:07 -0400

    Implemented new aspect and cleanup

--- src/main/kotlin/io/github/craigmiller160/fpresultkt/transaction/advice/spring/SpringResultTransactionAdvice.kt
+++ src/main/kotlin/io/github/craigmiller160/fpresultkt/transaction/advice/spring/SpringResultTransactionAdvice.kt
@@ -1,6 +1,21 @@
 package io.github.craigmiller160.fpresultkt.transaction.advice.spring
 
+import io.github.craigmiller160.fpresultkt.transaction.support.ResultTransactionSupport
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
 
-@Aspect @Component class SpringResultTransactionAdvice {}
+@Aspect
+@Component
+class SpringResultTransactionAdvice(
+    private val resultTransactionSupport: ResultTransactionSupport
+) {
+  @Pointcut("@annotation(org.springframework.transaction.annotation.Transactional)")
+  fun springTransactional() {}
+
+  @Around("springTransactional()")
+  fun handleResultReturnValue(joinPoint: ProceedingJoinPoint): Any? =
+      resultTransactionSupport.handleResultReturnValue(joinPoint)
+}
