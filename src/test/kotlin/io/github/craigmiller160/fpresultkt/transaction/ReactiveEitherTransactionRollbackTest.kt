7ecd5a04265c10e59bdebfbb1c3ef0ca4731b392
Craig Miller <craigmiller160@gmail.com>
2023-04-02 18:11:24 -0400

    Stubbed out new tests

---
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/transaction/ReactiveEitherTransactionRollbackTest.kt
@@ -0,0 +1,26 @@
+package io.github.craigmiller160.fpresultkt.transaction
+
+import io.github.craigmiller160.fpresultkt.BaseTest
+import org.junit.jupiter.api.Test
+
+class ReactiveEitherTransactionRollbackTest : BaseTest() {
+  @Test
+  fun `coroutine - commits the result of the transaction for Right`() {
+    TODO()
+  }
+
+  @Test
+  fun `coroutine - rolls back the transaction for Left`() {
+    TODO()
+  }
+
+  @Test
+  fun `reactor - commits the result of the transaction for Right`() {
+    TODO()
+  }
+
+  @Test
+  fun `reactor - rolls back the transaction for Left`() {
+    TODO()
+  }
+}
