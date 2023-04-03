d9f750a16edfb89f1b4c758208e181ca5332d4b3
Craig Miller <craigmiller160@gmail.com>
2023-04-03 16:16:03 -0400

    Added some test cleanup logic

--- src/test/kotlin/io/github/craigmiller160/fpresultkt/transaction/ReactiveEitherTransactionRollbackTest.kt
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/transaction/ReactiveEitherTransactionRollbackTest.kt
@@ -1,13 +1,17 @@
 package io.github.craigmiller160.fpresultkt.transaction
 
 import arrow.core.Either
+import arrow.core.flatMap
 import io.github.craigmiller160.fpresultkt.BaseTest
 import io.github.craigmiller160.fpresultkt.transaction.extensions.executeAndAwaitEither
+import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.matchers.shouldBe
 import java.util.UUID
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.r2dbc.core.DatabaseClient
@@ -17,6 +21,16 @@ import org.springframework.transaction.reactive.TransactionalOperator
 class ReactiveEitherTransactionRollbackTest : BaseTest() {
   @Autowired private lateinit var databaseClient: DatabaseClient
   @Autowired private lateinit var transactionOperator: TransactionalOperator
+
+  @BeforeEach
+  fun setup() {
+    runBlocking { clearDb() }
+  }
+
+  @AfterEach
+  fun cleanup() {
+    runBlocking { clearDb() }
+  }
   @Test
   fun `coroutine - commits the result of the transaction for Right`() {
     runBlocking {
@@ -28,7 +42,13 @@ class ReactiveEitherTransactionRollbackTest : BaseTest() {
 
   @Test
   fun `coroutine - rolls back the transaction for Left`() {
-    TODO()
+    runBlocking {
+      countRecords().shouldBe(0)
+      transactionOperator
+          .executeAndAwaitEither { executeInsert().flatMap { Either.Left(RuntimeException()) } }
+          .shouldBeLeft(RuntimeException())
+      countRecords().shouldBe(0)
+    }
   }
 
   @Test
@@ -46,6 +66,9 @@ class ReactiveEitherTransactionRollbackTest : BaseTest() {
           .sql("SELECT COUNT(*) AS the_count FROM person")
           .map { row, _ -> row.get("the_count").toString().toLong() }
           .awaitSingle()
+
+  private suspend fun clearDb() =
+      databaseClient.sql("DELETE FROM person").fetch().rowsUpdated().awaitSingle()
 
   private suspend fun executeInsert(): Either<Throwable, Long> =
       Either.catch {
