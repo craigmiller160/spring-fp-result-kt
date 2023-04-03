5ab4d8770b2bf42a8963f90747d7106319445b6c
Craig Miller <craigmiller160@gmail.com>
2023-04-03 16:14:28 -0400

    Got test to pass

--- src/test/kotlin/io/github/craigmiller160/fpresultkt/transaction/ReactiveEitherTransactionRollbackTest.kt
+++ src/test/kotlin/io/github/craigmiller160/fpresultkt/transaction/ReactiveEitherTransactionRollbackTest.kt
@@ -1,11 +1,17 @@
 package io.github.craigmiller160.fpresultkt.transaction
 
+import arrow.core.Either
 import io.github.craigmiller160.fpresultkt.BaseTest
+import io.github.craigmiller160.fpresultkt.transaction.extensions.executeAndAwaitEither
+import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.matchers.shouldBe
+import java.util.UUID
+import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.r2dbc.core.DatabaseClient
-import org.springframework.r2dbc.core.awaitOne
+import org.springframework.r2dbc.core.awaitSingle
 import org.springframework.transaction.reactive.TransactionalOperator
 
 class ReactiveEitherTransactionRollbackTest : BaseTest() {
@@ -14,14 +20,10 @@ class ReactiveEitherTransactionRollbackTest : BaseTest() {
   @Test
   fun `coroutine - commits the result of the transaction for Right`() {
     runBlocking {
-      databaseClient
-          .sql("SELECT COUNT(*) FROM person")
-          .map { row -> row.get(0).toString().toInt() }
-          .awaitOne()
-          .let { println("COUNT: $it") }
+      countRecords().shouldBe(0)
+      transactionOperator.executeAndAwaitEither { executeInsert() }.shouldBeRight(1)
+      countRecords().shouldBe(1)
     }
-
-    TODO()
   }
 
   @Test
@@ -38,4 +40,22 @@ class ReactiveEitherTransactionRollbackTest : BaseTest() {
   fun `reactor - rolls back the transaction for Left`() {
     TODO()
   }
+
+  private suspend fun countRecords(): Long =
+      databaseClient
+          .sql("SELECT COUNT(*) AS the_count FROM person")
+          .map { row, _ -> row.get("the_count").toString().toLong() }
+          .awaitSingle()
+
+  private suspend fun executeInsert(): Either<Throwable, Long> =
+      Either.catch {
+        databaseClient
+            .sql("INSERT INTO person (id, name, age) VALUES (:id, :name, :age)")
+            .bind("id", UUID.randomUUID())
+            .bind("age", 30)
+            .bind("name", "Name-${UUID.randomUUID()}")
+            .fetch()
+            .rowsUpdated()
+            .awaitSingle()
+      }
 }
